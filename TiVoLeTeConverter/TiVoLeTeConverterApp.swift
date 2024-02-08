051a9b9fc6dee1b5d3052637616ffb67325ae52f
Sergey Petrosyan <ipetriez@icloud.com>
2024-02-08 16:11:27 +0400

    Initial Commit

---
+++ TiVoLeTeConverter/TiVoLeTeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiVoLeTeConverterApp.swift
+//  TiVoLeTeConverter
+//
+//  Created by Sergey Petrosyan on 08.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TiVoLeTeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
